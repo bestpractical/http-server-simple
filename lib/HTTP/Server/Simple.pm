298d55369c12d53b4b1de1cf8b2a1ef3047b97c8
Jesse Vincent <jesse@bestpractical.com>
2005-10-10 23:33:33 +0000

    r17272@hualien:  jesse | 2005-10-10 15:32:15 -0400  * Updates to SIGHUP handler docs from Matt Trout
    git-svn-id: svn://svn.bestpractical.com/HTTP-Server-Simple@3946 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/HTTP/Server/Simple.pm
+++ lib/HTTP/Server/Simple.pm
@@ -55,7 +55,13 @@ By default, the server traps a few signals:
 
 =item HUP
 
-When you C<kill -HUP> the server, it does its best to rexec itself.
+When you C<kill -HUP> the server, it does its best to rexec
+itself.  Please note that in order to provide restart-on-SIGHUP,
+HTTP::Server::Simple sets a SIGHUP handler during initialisation. If
+your request handling code forks you need to make sure you reset this
+or unexpected things will happen if somebody sends a HUP to all running
+processes spawned by your app (e.g. by "kill -HUP <script>")
+
 
 =item PIPE
 
